1df33bc4557f528da2aa5c36cb70e821e85cf4da
alexlobko <92lobko@gmail.com>
2024-07-02 19:53:35 +0300

    add tests and requirements.txt

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,2 +1,72 @@
 from django.test import TestCase
+from django.contrib.auth.models import Permission
 
+from apis.models import Task
+from .models import CustomUser
+
+
+class CustomUserTestCase(TestCase):
+
+    def setUp(self):
+        self.user1 = CustomUser.objects.create(
+            username='user1',
+            full_name='User One',
+            position='Employee',
+        )
+
+        self.user2 = CustomUser.objects.create(
+            username='user2',
+            full_name='User Two',
+            position='Employee 2',
+            deputy=self.user1
+        )
+
+        self.user3 = CustomUser.objects.create(
+            username='user3',
+            full_name='User Three',
+            position='Manager',
+            deputy=self.user2
+        )
+        self.task1 = Task.objects.create(
+            content='Task One',
+            main_executor=self.user2,
+
+        )
+        self.task2 = Task.objects.create(
+            content='Task Two',
+            main_executor=self.user3,
+        )
+        self.task2.co_executors.set([self.user2])
+
+    def test_full_name_method(self):
+        self.assertEqual(self.user1.full_name, 'User One')
+
+    def test_position_method(self):
+        self.assertEqual(self.user1.position, 'Employee')
+
+    def test_deputy_method(self):
+        self.assertEqual(self.user1.deputy, None)
+        self.assertEqual(self.user2.deputy, self.user1)
+        self.assertEqual(self.user3.deputy, self.user2)
+
+    def test_main_tasks_method(self):
+        self.assertEqual(self.user1.main_tasks.all().count(), 0)
+        self.assertEqual(self.user2.main_tasks.all().count(), 1)
+        self.assertEqual(self.user3.main_tasks.all().count(), 1)
+
+    def test_co_executors_tasks_method(self):
+        self.assertEqual(self.user1.co_executors_tasks.all().count(), 0)
+        self.assertEqual(self.user2.co_executors_tasks.all().count(), 1)
+        self.assertEqual(self.user3.co_executors_tasks.all().count(), 0)
+
+    def test_str_method(self):
+        self.assertEqual(str(self.user1), 'User One')
+
+    def test_delete_method(self):
+        self.user2.delete()
+        self.assertFalse(CustomUser.objects.filter(pk=self.user2.id).exists())
+        self.user3.refresh_from_db()
+        self.assertEqual(self.user3.deputy, self.user1)
+        self.assertEqual(Task.objects.get(pk=self.task1.id).main_executor, self.user1)
+        self.assertNotIn(self.user2, Task.objects.get(pk=self.task2.id).co_executors.all())
+        self.assertIn(self.user1, Task.objects.get(pk=self.task2.id).co_executors.all())
--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,70 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from accounts.models import CustomUser
+from .models import Task, RegulatoryDocument
+
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        self.user1 = CustomUser.objects.create(username='user1')
+        self.user2 = CustomUser.objects.create(username='user2')
+        self.regulatory_doc = RegulatoryDocument.objects.create(
+            doc_type='Протокол',
+            date_approved=timezone.now().date(),
+            registration_number='123',
+            full_name='Test Regulatory Document'
+        )
+        self.task1 = Task.objects.create(
+            content='Task 1',
+            main_executor=self.user1,
+            deadline=timezone.now().date(),
+            regulation=self.regulatory_doc,
+            status='Исполнено'
+        )
+        self.task2 = Task.objects.create(
+            content='Task 2',
+            main_executor=self.user2,
+            deadline=timezone.now().date(),
+            regulation=self.regulatory_doc,
+            status='Не исполнено'
+        )
+
+
+class TaskTestCase(BaseTestCase):
+    def test_task_creation(self):
+        self.assertEqual(self.task1.content, 'Task 1')
+        self.assertEqual(self.task1.main_executor, self.user1)
+        self.assertEqual(self.task1.co_executors.count(), 0)
+        self.assertEqual(self.task1.deadline, timezone.now().date())
+        self.assertEqual(self.task1.regulation, self.regulatory_doc)
+        self.assertEqual(self.task1.status, 'Исполнено')
+
+    def test_task_update(self):
+        self.task1.status = 'Не исполнено'
+        self.task1.save()
+        self.assertEqual(self.task1.status, 'Не исполнено')
+
+    def test_task_update_with_refresh_from_db(self):
+        self.task1.status = 'Не исполнено'
+        self.task1.save()
+        self.task1.refresh_from_db()
+        self.assertEqual(self.task1.status, 'Не исполнено')
+
+
+class RegulatoryDocumentTestCase(BaseTestCase):
+    def test_regulatory_document_creation(self):
+        self.assertEqual(self.regulatory_doc.doc_type, 'Протокол')
+        self.assertEqual(self.regulatory_doc.date_approved, timezone.now().date())
+        self.assertEqual(self.regulatory_doc.registration_number, '123')
+        self.assertEqual(self.regulatory_doc.full_name, 'Test Regulatory Document')
+        self.assertFalse(self.regulatory_doc.status)
+
+    def test_regulatory_document_update_status(self):
+        self.regulatory_doc.update_status()
+        self.assertFalse(self.regulatory_doc.status)
+
+        self.task2.status = 'Исполнено'
+        self.task2.save()
+        self.regulatory_doc.update_status()
+        self.assertTrue(self.regulatory_doc.status)
